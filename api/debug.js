ffd7fcea77420bccd57f51e33c89d48f9aed3dd6
Aurélie <aurelie.gautier@bain.com>
2026-05-11 11:18:59 +0200

    Remove debug endpoint

--- api/debug.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  res.json({
-    hasKey: !!process.env.ANTHROPIC_API_KEY,
-    keyLength: (process.env.ANTHROPIC_API_KEY || '').length,
-    nodeEnv: process.env.NODE_ENV,
-  })
-}
